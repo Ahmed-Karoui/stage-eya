37cd0c8396d20011c4e9707d81fbb568f229d708
Ahmed Karoui <69198798+Ahmed-Karoui@users.noreply.github.com>
2025-10-01 13:05:40 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
+// db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+let pool;
+
+if (!global._mysqlPool) {
+  global._mysqlPool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306, // <-- ensure port used
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -10,6 +15,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
+  });
+}
+
+pool = global._mysqlPool;
 
 module.exports = pool;
